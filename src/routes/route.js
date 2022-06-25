7beacafb29c794fa47ec0385c234a648e3ef9ae3
Bhuvnesh Sharma <104725390+bhuvi844964@users.noreply.github.com>
2022-06-25 17:57:48 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,14 +3,17 @@ const router = express.Router();
 
 const AuthController= require("../controllers/authorcontroller")
 const BlogsController= require("../controllers/blogscontroller")
+const MW= require("../middleware/auth")
 
 
 router.post("/authors", AuthController.createAuthor )
-router.post("/blogs", BlogsController.createBlogs )
-router.get("/blogs", BlogsController.getBlogs )
-router.put("/blogs/:blogId", BlogsController.updateBlogs )
-router.delete("/blogs/:blogId", BlogsController.deleteBlogs )
-router.delete("/blogs", BlogsController.queryDeleted )
+router.post("/blogs",MW.authenticate, BlogsController.createBlogs )
+router.get("/blogs",MW.authenticate, BlogsController.getBlogs )
+router.put("/blogs/:blogId",MW.authenticate,MW.authorise, BlogsController.updateBlogs )
+router.delete("/blogs/:blogId",MW.authenticate,MW.authorise, BlogsController.deleteBlogs )
+router.delete("/blogs",MW.authenticate,MW.authorise, BlogsController.queryDeleted )
+
+router.post("/login", AuthController.loginAuthor)
 
 module.exports = router; 
 
