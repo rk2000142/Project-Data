02c1a71962725b3ca19d00b6a1fb5ed5977a79ef
linagodbole98 <linagodbole98@gmail.com>
2022-07-25 12:37:18 +0530

    index

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,1 +1,12 @@
-express= require("express")
+const express = require("express");  //post,get 
+const router = express.Router();
+const { userlogin } = require("../controllers/userController");
+
+ 
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+}) 
+module.exports = router;
+  
+    
+
