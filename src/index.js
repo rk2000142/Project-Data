718adb2d1e02276e5cf130ae6238a019db18a2ca
Souvik Jha <jhasouvik4@gmail.com>
2022-07-25 12:24:52 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-express= require("express")
+const express = require('express');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const mongoose = require('mongoose');
+const app=express();
+
+app.use(bodyParser.json());
+app.use(multer().any()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://Souvik_Jha:Govtjob1@cluster0.zb2phjl.mongodb.net/group23Database", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+
+app.listen(process.env.PORT || 3001, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3001))
+});
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,1 +1,16 @@
-express= require("express")
+const express = require('express');
+const router = express.Router();
+const userController=require("../controllers/userController")
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
